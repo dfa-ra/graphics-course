bb5ead41ed31c66bdddddefbd7f4f9b866be7fb2
ra <89213748002t@gmail.com>
2025-10-25 18:02:11 +0300

    feat: lab1 - add picture

--- lab1/config.py
+++ lab1/config.py
@@ -1,5 +1,6 @@
 IMAGE_PATHS = [
     "source/img1.jpg",
     "source/img2.jpg",
+    "source/img3.jpg",
     "source/img.jpg"
 ]
--- lab2/filters/enhance_filters.py
+++ lab2/filters/enhance_filters.py
@@ -4,12 +4,36 @@ from .base import FilterRegistry
 
 @FilterRegistry.register("Brightness")
 def f_brightness(img, brightness=1.0, **_):
-    return ImageEnhance.Brightness(img).enhance(brightness)
+    img = img.convert("RGB")  # на всякий случай
+    pixels = img.load()
+    w, h = img.size
+
+    for y in range(h):
+        for x in range(w):
+            r, g, b = pixels[x, y]
+            r = min(255, max(0, int(r * brightness)))
+            g = min(255, max(0, int(g * brightness)))
+            b = min(255, max(0, int(b * brightness)))
+            pixels[x, y] = (r, g, b)
+
+    return img
 
 
 @FilterRegistry.register("Contrast")
 def f_contrast(img, contrast=1.0, **_):
-    return ImageEnhance.Contrast(img).enhance(contrast)
+    img = img.convert("RGB")
+    pixels = img.load()
+    w, h = img.size
+
+    for y in range(h):
+        for x in range(w):
+            r, g, b = pixels[x, y]
+            r = min(255, max(0, int((r - 128) * contrast + 128)))
+            g = min(255, max(0, int((g - 128) * contrast + 128)))
+            b = min(255, max(0, int((b - 128) * contrast + 128)))
+            pixels[x, y] = (r, g, b)
+
+    return img
 
 
 @FilterRegistry.register("Blur")
